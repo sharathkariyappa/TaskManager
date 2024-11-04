59ef24b75575b3c760c8dd067672f0e5652b48b4
Krupa <sharathkariyappa98@gmail.com>
2024-11-04 16:24:34 +0530

    Added Task Functionalities

---
+++ controllers/taskController.js
@@ -0,0 +1,37 @@
+const Task = require('../models/Task');
+
+exports.createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.updateTask = async (req, res) => {
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(task);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.deleteTask = async (req, res) => {
+    try {
+        await Task.findByIdAndDelete(req.params.id);
+        res.status(204).end();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
